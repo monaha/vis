d0a02453b3c3d967cb0d6214802e04a3e8284cfc
monaha <mo_om1993@hotmaiil.com>
2016-04-12 17:45:19 -0700

    done

---
+++ multimediaProject/homepag.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class homepag : Form
+    {
+        static String user_name;
+        public homepag()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+             user_name = textBox1.Text;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {this.Hide();
+            Form category = new Form();
+            
+            category.Show();
+            
+        }
+    }
+}
